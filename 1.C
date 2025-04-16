2136f0dcdcb2db55611327066133826484ca619e
лисс <samuranovaelizaveta@gmail.com>
2025-04-16 08:56:17 +0300

    Update 1.C

--- 1.C
+++ 1.C
@@ -6,18 +6,20 @@ int main() {
  char letter = 'g';
  int index = -1;
 
- for (int i = 0; i < strlen(alphabet); i++) {
-  if (alphabet[i] == letter) {
+ int alphabet_size = sizeof(alphabet) / sizeof(alphabet[0]);
+
+ for (int i = 0; i < alphabet_size; i++) {
+   if (alphabet[i] == letter) {
   index = i + 1; 
   break; 
   }
  }
 
  if (index != -1) {
-  printf("Буква '%c' находится на позиции %d в алфавите\n", letter, index);
-  } 
+ printf("Буква '%c' находится на позиции %d в алфавите\n", letter, index);
+ } 
  else {
-  printf("Буква '%c' не найдена в алфавите\n", letter);
-  }
- return 0;
+ printf("Буква '%c' не найдена в алфавите\n", letter);
+ }
+return 0;
 }
